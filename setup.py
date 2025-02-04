ced3ed392344456aa51e00432bfab6fa49606957
T-Yashwanth <yashwanthtelukuntla@gmail.com>
2025-02-03 22:13:34 -0600

    small change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="PDF417_Scanner",
     version="0.2",
     author="Yash",
-    description="this is the version 0.1 that has flask api and front end for only image scanning and returning the data",
+    description="this is the version 0.2 that has flask api and front end for image scanning, video scanning and returning the data",
     url="https://github.com/T-Yashwanth/PDF417-Scanner",
     packages=find_packages(),
     package_dir={"": "src"},
--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ import os
 from flask_main import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))
-    app.run(host= "0.0.0.0", port= port)
+    #port = int(os.environ.get("PORT", 4000))
+    app.run()
